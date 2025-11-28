e29d50f3e4fa21a2008bfb343076724c6c414c47
apptie <appti@naver.com>
2025-11-28 14:34:00 +0900

    refactor: Calculator 불필요한 방어 로직 삭제

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -12,24 +12,16 @@ public class Calculator {
 
     public BigDecimal calculate(String expression) {
         String[] tokens = expressionTokenizer.tokenize(expression);
-        BigDecimal leftOperand = parseNumber(tokens[0]);
+        BigDecimal leftOperand = new BigDecimal(tokens[0]);
 
         for (int i = 1; i < tokens.length - 1; i += 2) {
             String operatorToken = tokens[i];
             Operator operator = Operator.find(operatorToken);
-            BigDecimal rightOperand = parseNumber(tokens[i + 1]);
+            BigDecimal rightOperand = new BigDecimal(tokens[i + 1]);
 
             leftOperand = operator.calculate(leftOperand, rightOperand);
         }
 
         return leftOperand;
-    }
-
-    private BigDecimal parseNumber(String number) {
-        try {
-            return new BigDecimal(number);
-        } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("유효한 수식이 아닙니다.", ex);
-        }
     }
 }
